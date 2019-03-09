13c89bcffac35297e7c8f37ef04d515995141d5b
JCIII <joseph.tallieu@gmail.com>
2019-03-09 02:47:12 -0600

    Index page to demo

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from views import index_view
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v2/hollabacks/', include('stefani.urls')),
-    
+    url(r'^$', RedirectView.as_view(url='/static/AdminLTE/index.html', permanent=False))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
